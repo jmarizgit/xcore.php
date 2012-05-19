8a3882b1b95dfe36633f803f22c7aa0f7e127f99
Mariz Melo <jmarizsf@gmail.com>
2012-05-19 09:34:04 -0700

    Creating xcore JavaScript file
    All necessary javascript to handle features for xcore will be deployed
    here

---
+++ xcore/js/xcore.js
@@ -0,0 +1,12 @@
+/*
+	@type: JAVASCRIPT FILE
+	@author: MARIZ MELO
+	@company: XCHEMA
+	@release: 05/18/2012
+	@dependencies: JQuery
+*/
+$(function(){
+	$(".xc_debug").on("click", function(){
+		$(this).slideUp('fast');
+	});
+});

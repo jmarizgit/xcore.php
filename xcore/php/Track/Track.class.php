dc56ccc9ddec948f29db56b100fc6285fa093a4d
Mariz Melo <jmarizsf@gmail.com>
2012-05-19 09:38:29 -0700

    Better message handling

--- xcore/php/Track/Track.class.php
+++ xcore/php/Track/Track.class.php
@@ -1,8 +1,34 @@
 <?php
+/*
+	@type: CLASS
+	@author: JOSE MARIZ MELO
+	@company: XCHEMA
+	@release: 07/18/2010 
+	@updated: 05/18/2012
+	@description: This class is uniquely to display messages on the system
+*/
 
-//CLASS TO DEAL WITH STRINGS
+//THIS CLASS ALLOW ME TO WRITE DEBUG MESSAGES ON THE SCREEN
 class Track extends Debug{
-	//JUST TO USE THE METHODS/ATTRIBUTES FOR THE DEBUG CLASS
-}
+
+	/*INITILIZES CLASS WITH ACCESS TO DEBUG SYSTEM*/
+	function __construct($debug=0, $debugphp=0){
+		
+		if($debug){
+			$this->debugSTART($debugphp);
+			$this->debugMESSAGE('S', 'TRACK object created');
+		}
+		return '';
+		
+	}
+	
+	/*SHOW HELP FROM DEBUG CLASS (SUPERCLASS)*/
+	function __toString(){
+		
+		return $this->superHELP();
+		
+	}//end:HELP
+	
+}//class
 
 ?>

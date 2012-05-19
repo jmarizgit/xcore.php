3d8ed7eeaee561ec0a85cec76be11ebc78b08df7
Mariz Melo <jmarizsf@gmail.com>
2012-05-19 09:42:06 -0700

    Update to support "systems" feature
    Systems is a built in feature that enables xcore to provide different
    functionalities. In this release I stated to develop a simple blog
    system.

--- index.php
+++ index.php
@@ -1,34 +1,45 @@
 <?php 
-
-/*@type: XCORE PHP FRAMEWORK */
-/*@author: MARIZ MELO */
-/*@company: XCHEMA */
-/*@release: 01-10-2011 */
-/*@url: http://github.com/jmarizgit/XCORE */
-/*@required: xcore/php/xcore.php */
-
+/*
+	@title: XCORE PHP FRAMEWORK
+	@author: MARIZ MELO
+	@company: XCHEMA
+	@release: 01/10/2011
+	@url: http://github.com/jmarizgit/xcore.php 
+	@required: xcore/php/xcore.php
+*/
 //REQUIRED
-include('xcore/php/xcore.php');
+include('./xcore/php/xcore.php');
+
+
+//you can set your own messages
+$info = new Track();
+
 
 //load website/project basic configuration
 $conf = new Configure();
-//echo $conf; //try uncomment this line
 
-$info = new Track(1,1);
+
+$data = new Database();
 
 
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$conf->lang?>" lang="<?=$conf->lang?>">
+<html lang="<?=$conf->lang?>">
 <head>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="Cache-Control" content="no-cache"/> 
 	
+	<!-- MOBILE -->
+	<meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0; target-densitydpi=device-dpi"/>
+	
+	<!-- CHROME FRAME for IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
 	<meta name="description" content="description of your page/system">
 	<meta name="keywords" content="keywords list">
 	<meta name="author" content="page author name">
 	<meta name="publisher" content="page publisher name">
-	<meta name="copyright" content="copyright(c) 2011 Project name">
+	<meta name="copyright" content="copyright(c) <?=date('Y')?> Project name">
 	<meta name="distribution" content="Global">
 	<meta name="robots" content="index, follow">
 	<meta name="audience" content="all">
@@ -36,31 +47,32 @@ $info = new Track(1,1);
 	<meta name="language" content="English">
 
 	<title><?=$conf->title?></title>
-	
-	<!-- INSERT YOUR LOCAL IP/folder_to_your_root_project, ONLINE, insert your website address -->
-
 
 	<!-- set your own image -->
-	<link rel="shortcut icon" href="xcore/img/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
 	
 	<!-- insert your own style for the page here -->
-	<link rel="stylesheet" type="text/css" href="xcore/css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="xcore/css/magritte.css" />
+	<link rel="stylesheet" type="text/css" href="xcore/css/blog.css" />
 
 </head>
 
-<body> 
-	
-	<?=$info->debugMESSAGE('S', 'See the <a href="README.txt">README.TXT</a> file for more information, visit the <a href="http://github.com/jmarizgit/XCORE"><i>gitHUB page for the project</i></a>. Thank you for using XCORE!')?>
-	<!-- JUST DELETE THIS LINE -->
+<body>
 
-	<div id="content">
-		<div id="version">
-			<h2>V0.1b</h2>
-		</div>
-	</div>
 	
-	<div id="footer" class="align_center">Created by <a href="http://www.emoriz.com">Mariz Melo.</a></div>
+	<?=$info->debugMESSAGE('S', 'See the <a href="README.txt">README.TXT</a> file for more information, visit the <a href="http://github.com/jmarizgit/xcore.php">gitHUB page for the project</a>. Thank you for using XCORE!')?>
+	
+	
+	<?php include("./xcore/system/blog/index.html"); ?>
+	
+	
+	<!-- SCRIPTS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="./xcore/js/xcore.js"></script>
+	
+	
+	<!-- DELETE -->
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 	
 </body>
-
 </html>
